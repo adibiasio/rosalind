26aa3e50c62765d1c8b5a7003144ff6bf0ed5f3f
Andrew DiBiasio <mbox_user-github@yahoo.com>
2023-01-16 22:33:32 -0500

    organized files

---
+++ problems/reverse_complement.py
@@ -0,0 +1,19 @@
+"""Generates the Reverse Complement of a Strand of DNA.
+"""
+
+base_pairing = {
+    "A": "T",
+    "T": "A",
+    "C": "G",
+    "G": "C"
+}
+
+with open("in/rosalind_revc.txt", "r") as infile:
+    dna = infile.readlines()[0].strip()
+    complement = ""
+
+    for i in range(len(dna) - 1, -1, -1):
+        complement += base_pairing[dna[i]]
+    
+    with open("out.txt", "w") as out:
+        out.write(complement)
